448ed878e5afdce1f556eec2dedd1bd6ace7ee7c
Kukkis66 <Kukkis66@github.com>
2023-04-28 17:09:25 +0300

    kissa liikkuu oikealle ja vasemmalle

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ class Kissapeli:
     def __init__(self, korkeus, leveys) -> None:
         pygame.init()
 
-        #self.lataa_kuvat()
+        self.lataa_kuvat()
+        
+        self.kissa_x = 0
+        self.kissa_y = leveys-self.kuvat[0].get_height()
+        self.kissan_nopeus = 3
+
+        self.oikealle = False
+        self.vasemmalle = False
+
         self.korkeus = korkeus
         self.leveys = leveys
         self.naytto = pygame.display.set_mode((self.korkeus, self.leveys))
@@ -14,32 +22,54 @@ class Kissapeli:
     
         self.run()
 
-    def lataa_kuvat(self):      #lataa peliin kuvat
+
+
+    #lataa peliin kuvat
+    def lataa_kuvat(self):      
         self.kuvat = []
-        for kuva in []:
-            self.kuvat.append(pygame.image.load(kuva +".png"))
+        for kuva in ["kissa1", "kissa1_puna", "kissa1_puna_miau", "kolmio"]:
+            self.kuvat.append(pygame.image.load("images/"+ kuva +".png"))
 
 
-    def uusi_peli(self):
-        pass
+    
+
+        
 
 
-
-    def run(self):      #silmukka mikä pitää pelin käynnissä
+    #silmukka mikä pitää pelin käynnissä
+    def run(self):      
         while True:
             self.tapahtumat()
             self.piirra_naytto()
 
-
-    def tapahtumat(self):   #metodi mikä havannoi mitä painetaan
+    #metodi mikä havannoi mitä painetaan
+    def tapahtumat(self):   
         for tapahtuma in pygame.event.get():
+            if tapahtuma.type == pygame.KEYDOWN:
+                if tapahtuma.key == pygame.K_LEFT:
+                     self.vasemmalle = True
+                if tapahtuma.key == pygame.K_RIGHT:
+                     self.oikealle = True
+            if tapahtuma.type == pygame.KEYUP:
+                if tapahtuma.key == pygame.K_LEFT:
+                     self.vasemmalle = False
+                if tapahtuma.key == pygame.K_RIGHT:
+                     self.oikealle = False
             if tapahtuma.type == pygame.QUIT:
                 exit()
+        #liikuttaa kissaa oikealle tai vasemmalle
+        if self.oikealle:
+            self.kissa_x += self.kissan_nopeus
+        if self.vasemmalle:
+            self.kissa_x -= self.kissan_nopeus
 
 
-    def piirra_naytto(self):        #piirtää itse pelin ja kaiken sisällön siihen
-        self.naytto.fill((0,0,0))
+    #piirtää itse pelin ja kaiken sisällön siihen
+    def piirra_naytto(self):        
+        self.naytto.fill((255,255,255))
+        self.naytto.blit(self.kuvat[0], (self.kissa_x, self.kissa_y))
+
         pygame.display.flip()
 
-
-Kissapeli(1024,768) #käynnistää pelin sillä resoluutiolla mikä arvoksi annetaan
+#käynnistää pelin sillä resoluutiolla mikä arvoksi annetaan
+Kissapeli(1024,768) 
